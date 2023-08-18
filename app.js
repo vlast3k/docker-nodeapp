32b5d6f9aab8f9a4e4166a7ddf63273909fd58b2
Vladimir Savchenko <vladimir.savchenko@sap.com>
2023-08-18 07:46:52 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const approuter = require("@sap/approuter");
+const http = require('http');
 
-const app = approuter();
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
 
-app.start();
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received');
+  console.log('Waiting for 5 minutes...');
+  
+  setTimeout(() => {
+    console.log('5 minutes elapsed. Shutting down gracefully...');
+    server.close(() => {
+      console.log('Server closed');
+      process.exit(0);
+    });
+  }, 5 * 60 * 1000); // 5 minutes
+});
